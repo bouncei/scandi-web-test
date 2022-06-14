0bb4c89f4438379af707c3500f0c315b3edc06d8
bouncei <joshuainyang255@gmail.com>
2022-06-14 02:21:16 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,35 +1,26 @@
-import * as React from 'react'
+import * as React from "react";
 // import { BrowserRouter } from "react-router-dom";
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from "./App"
-import reportWebVitals from "./reportWebVitals"
-import { ApolloProvider } from '@apollo/client';
-import client from './server';
-import { Provider } from 'react-redux'
-import store from './Redux/store';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider } from "@apollo/client";
+import client from "./server";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <Provider store={store}>
-
-
         <App />
       </Provider>
-
-
     </ApolloProvider>
-
-
   </React.StrictMode>
 );
-
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
